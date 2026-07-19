a80e12e0de7fc780ca3666a1ce6e256f3bea2690
Chamuxx <ricardobarrett565@gmail.com>
2026-07-20 01:14:32 +0530

    Fix map 401 authentication error by using CartoDB

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -85,14 +85,14 @@ function ThemedTileLayer() {
   const { theme } = useTheme();
   return theme === "light" ? (
     <TileLayer
-      url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-      attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>'
+      url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
       maxZoom={20}
     />
   ) : (
     <TileLayer
-      url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-      attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>'
+      url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
       maxZoom={20}
     />
   );
